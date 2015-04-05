e44b42624dbfa734692d934419a7618ad51148c7
Dolan Miu <dolan_miu@hotmail.com>
2015-04-05 04:22:54 +0100

    added wind background

--- src/components/player/services/player.service.js
+++ src/components/player/services/player.service.js
@@ -1,5 +1,5 @@
 /*globals angular */
-angular.module('lolApi').service('playerService', function ($timeout, birdBgIntrumentService) {
+angular.module('lolApi').service('playerService', function ($timeout, birdBgIntrumentService, soundFxIntrumentService) {
     'use strict';
 
     function addToQueue(event) {
@@ -15,7 +15,7 @@ angular.module('lolApi').service('playerService', function ($timeout, birdBgIntr
             var index = Math.floor(Math.random() * 43) + 1;
             birdBgIntrumentService.songs[index].play();
             console.log('playing ' + event.eventType + ' at: ' + event.timestamp);
-        }, event.timestamp);
+        }, event.timestamp / 5);
     }
 
     this.play = function (events) {
@@ -23,5 +23,8 @@ angular.module('lolApi').service('playerService', function ($timeout, birdBgIntr
         for (i = 0; i < events.length; i += 1) {
             addToQueue(events[i]);
         }
+        soundFxIntrumentService.wind.play();
+        soundFxIntrumentService.wind.loop = true;
+        soundFxIntrumentService.wind.volume = 0.3;
     };
 });
--- src/components/soundLibraries/services/soundFx.instrument.js
+++ src/components/soundLibraries/services/soundFx.instrument.js
@@ -3,4 +3,6 @@ angular.module('lolApi').service('soundFxIntrumentService', function (ngAudio) {
     'use strict';
 
     this.kick = ngAudio.load("assets/sounds/cajon.wav");
+    this.wind = ngAudio.load("assets/sounds/fx/wind.wav");
+
 });
