2c240295b99577e327278ea1b68aa88ca42d630b
Dolan Miu <dolan_miu@hotmail.com>
2015-04-11 19:14:07 +0100

    added path require

--- web.js
+++ web.js
@@ -1,6 +1,7 @@
 /*globals require */
 var compression = require('compression');
 var express = require('express');
+var path = require('path');
 var app = express();
  
 app.use(express.static(path.join(__dirname, '/dist')));
