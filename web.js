22e977547bd5c91015662141cdcda6e5757bbc7b
Dolan Miu <dolan_miu@hotmail.com>
2015-04-11 19:26:40 +0100

    added express middleware

--- web.js
+++ web.js
@@ -2,7 +2,27 @@
 var compression = require('compression');
 var express = require('express');
 var path = require('path');
+var favicon = require('static-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
 var app = express();
- 
+
+app.use(favicon());
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(cookieParser());
+
 app.use(express.static(path.join(__dirname, '/dist')));
+
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
+
 app.listen(process.env.PORT || 5000);
