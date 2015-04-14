1865bb941927a600d1de23964cdfb776120bcb5f
Dolan Miu <dolan_miu@hotmail.com>
2015-04-14 14:09:31 +0100

    UI fixes

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -1,6 +1,8 @@
 /*globals angular */
 angular.module('lolApi').controller('MainCtrl', function ($scope, colourService) {
     'use strict';
+    
+    $scope.enviromentMix = 50;
 
     $scope.getColour = function (value) {
         if (!angular.isDefined(value)) {
