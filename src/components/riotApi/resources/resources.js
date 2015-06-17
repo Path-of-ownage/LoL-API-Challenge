6edabf0c3325d051013dcdd4199230d350474d9b
Dolan Miu <dolan_miu@hotmail.com>
2015-06-17 10:43:28 +0100

    added new summoner resource

--- src/components/riotApi/resources/resources.js
+++ src/components/riotApi/resources/resources.js
@@ -8,3 +8,9 @@ angular.module('lolApi').factory('matchResource', function ($resource, apiServic
     'use strict';
     return $resource('https://euw.api.pvp.net/api/lol/euw/v2.2/match/:matchId?includeTimeline=true&api_key=' + apiService.getApiKey());
 });
+
+angular.module('lolApi').factory('summonerResource', function ($resource, apiService) {
+    'use strict';
+    return $resource('https://:region.api.pvp.net/api/lol/:region/v1.4/summoner/by-name/:username?api_key=' + apiService.getApiKey());
+});
+
